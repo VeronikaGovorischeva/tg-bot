ec92a86e02be8b0042a4a7e99b24b8edad7039ec
YuriiHauss <danila.k3726@gmail.com>
2025-04-29 18:05:11 +0300

    saves data in ukrainian

--- data.py
+++ data.py
@@ -3,13 +3,12 @@ import json
 
 def load_data(file, *args):
     try:
-        with open(file, 'r') as f:
+        with open(f"{file}", 'r',encoding='utf-8') as f:
             return json.load(f)
     except FileNotFoundError:
         return args[0] if args else {}
 
 
 def save_data(data, file):
-    with open(file, 'w') as f:
-        json.dump(data, f, indent=4)
-
+    with open(file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
